314ad6b9ef22a6175b3ee90c94cd7afcf9d89348
Mina Guirguis <MGuirguis.10@gmail.com>
2018-03-02 17:59:41 -0500

    Initial Commit

---
+++ NewNotifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewNotifications
+//
+//  Created by Mina Guirguis on 3/2/18.
+//  Copyright © 2018 Mina Guirguis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
